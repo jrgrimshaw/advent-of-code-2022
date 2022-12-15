520211461fbeb9205e6f0f93db5672001c7551fc
James Grimshaw <jamesrichardgrimshaw@gmail.com>
2022-12-15 12:15:08 +0000

    day8 pt2

--- day08/day08.py
+++ day08/day08.py
@@ -2,14 +2,7 @@ def readFile(fileName):
   with open(fileName) as file:
     return file.read()
 
-def main():
-  file = readFile("input.txt")
-
-  matrix = []
-  matrix = file.split("\n")
-  for i,row in enumerate(matrix):
-    matrix[i] = [int(x) for x in list(row)]
-
+def partOne(matrix):
   count = 0
 
   for i, row in enumerate(matrix):
@@ -17,28 +10,89 @@ def main():
       if i == 0 or j == 0 or i == len(matrix)-1 or j == len(row)-1 :
         count += 1
       else:
-        visibility = {'n': True, 's': True, 'e': True, 'w': True}
-
+        visible = True
         for r in range(i):
           if matrix[r][j] >= matrix[i][j]:
-            visibility['n'] = False
+            visible = False
+        if visible:
+          count += 1
+          continue;
 
+        visible = True
         for r in range(len(matrix)-i-1):
           if matrix[r+i+1][j] >= matrix[i][j]:
-            visibility['s'] = False
+            visible = False
+        if visible:
+          count += 1
+          continue;
 
+        visible = True
         for c in range(j):
           if matrix[i][c] >= matrix[i][j]:
-            visibility['w'] = False
+            visible = False
+        if visible:
+          count += 1
+          continue;
 
+        visible = True
         for c in range(len(row)-j-1):
           if matrix[i][c+j+1] >= matrix[i][j]:
-           visibility['e'] = False
-
-        if visibility['n'] or visibility['s'] or visibility['e'] or visibility['w']:
+           visible = False
+        if visible:
           count += 1
+          continue;
 
-  print(count)
+  return count
+
+def partTwo(matrix):
+  scores = []
+
+  for i, row in enumerate(matrix):
+    for j, col in enumerate(row):
+
+      north = 0
+      south = 0
+
+      if i > 0 and i < len(matrix)-1:
+        for n in range(i-1, -1, -1):
+          north += 1
+          if matrix[n][j] >= matrix[i][j]:
+            break;
+
+        for s in range(i+1, len(matrix), 1):
+          south += 1
+          if matrix[s][j] >= matrix[i][j]:
+            break;
+
+      west = 0
+      east = 0
+
+      if j > 0 and j < len(row)-1:
+        for w in range(j-1, -1, -1):
+          west += 1
+          if matrix[i][w] >= matrix[i][j]:
+            break;
+
+        for e in range(j+1, len(row), 1):
+          east += 1
+          if matrix[i][e] >= matrix[i][j]:
+            break;
+
+      scores.append(north * south * east * west)
+
+  return max(scores)
+
+def main():
+  file = readFile("input.txt")
+
+  # Build matrix
+  matrix = []
+  matrix = file.split("\n")
+  for i,row in enumerate(matrix):
+    matrix[i] = [int(x) for x in list(row)]
+
+  print(partOne(matrix))
+  print(partTwo(matrix))
 
 if __name__ == "__main__":
   main()
